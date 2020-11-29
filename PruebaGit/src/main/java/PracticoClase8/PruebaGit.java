f60e90a13138b5c2306fcdf2905dfe14892ecc05
Diego Barletta <diegohbarletta@gmail.com>
2020-11-29 15:03:16 -0300

    commit de sourcetree

--- PruebaGit/src/main/java/PracticoClase8/PruebaGit.java
+++ PruebaGit/src/main/java/PracticoClase8/PruebaGit.java
@@ -3,6 +3,7 @@ package PracticoClase8;
 public class PruebaGit {
     public static void main(String args[]){
         System.out.println("Este codigo se va a subir a git");
+        System.out.println("Este nuevo codigo tambien se va a subir a git");
     }
 
 }
