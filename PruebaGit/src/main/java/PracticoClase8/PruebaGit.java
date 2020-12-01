131a612d5011ba826e8e5a6c540af36a89132144
Diego Barletta <diegohbarletta@gmail.com>
2020-11-30 22:24:13 -0300

    Nuevo proyecto, subido primero a dev (por error) ahora actualizado en el ejercicio 3 del practico 8

--- PruebaGit/src/main/java/PracticoClase8/PruebaGit.java
+++ PruebaGit/src/main/java/PracticoClase8/PruebaGit.java
@@ -4,6 +4,8 @@ public class PruebaGit {
     public static void main(String args[]){
         System.out.println("Este codigo se va a subir a git");
         System.out.println("Este nuevo codigo tambien se va a subir a git");
+
+        System.out.println("Este nuevo codigo quiero subir a branch ej3 de git");
     }
 
 }
